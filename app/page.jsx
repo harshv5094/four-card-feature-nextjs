d2ea7821217956b804acefa3411aa2fb4bc41242
Harsh Vyapari <harshv5094@gmail.com>
2024-02-03 11:50:00 +0530

    feat: Adding top level text

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,20 @@
-
 export default function Home() {
   return (
-    <main className="">
-      <div className="flex justify-center mt-20">
-        <p className="space-y-3 max-mobile:space-y-1 text-center text-very-dark-blue">
-          <h1 className="text-3xl max-mobile:text-2xl font-extralight">Reliable, efficient delivery</h1>
-          <h1 className="text-3xl max-mobile:text-2xl font-semibold">Powered By Technology</h1>
-          <h1 className="mx-[25em] max-laptop:mx-[20em] mt-10 max-mobile:mx-4 text-grayish-blue">
-            Our Artificial intelligence powered tools use millions of project data points to ensure that your project is successful
-          </h1>
-        </p>
+    <main>
+      <div className='flex flex-col items-center mt-16 text-very-dark-blue space-y-2 max-mobile:space-y-1 text-center '>
+        <h1 className='text-3xl max-mobile:text-2xl font-extralight'>
+          Reliable, efficient delivery
+        </h1>
+        <h1 className='text-3xl max-mobile:text-2xl font-bold'>
+          Powered by Technology
+        </h1>
+        <div className='mt-10 mx-[25em] max-laptop:mx-[10em] max-tablet:mx-10 max-mobile:mx-5 text-grayish-blue'>
+          Our Artificial Intelligence powered tools use millions of project data
+          points to ensure that your project is successful
+        </div>
+
+        <div></div>
       </div>
     </main>
-  );
+  )
 }
